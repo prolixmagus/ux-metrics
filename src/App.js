46312d396c0766b662017e81927846ca70910732
prolixmagus <jeremypfishman@gmail.com>
2025-01-23 11:03:50 -0800

    Add react-markdown and remove unused imports

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import Outcomes from "./pages/Outcomes";
 import Teams from "./pages/Teams";
 import Topics from "./pages/Topics";
 
-import Cohort1 from "./pages/Cohort1";
-import Cohort1Schedule from "./pages/Cohort1Schedule";
-import Cohort2 from "./pages/Cohort2";
-import Cohort2Schedule from "./pages/Cohort2Schedule";
-import Cohort3 from "./pages/Cohort3";
-import Cohort3Schedule from "./pages/Cohort3Schedule";
-import Cohort4 from "./pages/Cohort4";
-import Cohort4Schedule from "./pages/Cohort4Schedule";
 import Cohort5 from "./pages/Cohort5";
 import Cohort5Schedule from "./pages/Cohort5Schedule";
 import Cohort6 from "./pages/Cohort6";
@@ -30,42 +22,11 @@ function App() {
                 <Route path="/teams" element={<Teams />} />
                 <Route path="/topics" element={<Topics />} />
 
-
-                <Route path="/cohort1" element={<Cohort1 />} />
-                <Route 
-                    path="/cohort1-schedule" 
-                    element={<Cohort1Schedule />} 
-                />
-                
-                <Route path="/cohort2" element={<Cohort2 />} />
-                <Route
-                    path="/cohort2-schedule"
-                    element={<Cohort2Schedule />}
-                />
-
-                <Route path="/cohort3" element={<Cohort3 />} />
-                <Route
-                    path="/cohort3-schedule"
-                    element={<Cohort3Schedule />}
-                />
-
-                <Route path="/cohort4" element={<Cohort4 />} />
-                <Route 
-                    path="/cohort4-schedule" 
-                    element={<Cohort4Schedule />} 
-                />
-
                 <Route path="/cohort5" element={<Cohort5 />} />
-                <Route 
-                    path="/cohort5-schedule" 
-                    element={<Cohort5Schedule />} 
-                />
+                <Route path="/cohort5-schedule" element={<Cohort5Schedule />} />
 
                 <Route path="/cohort6" element={<Cohort6 />} />
-                <Route 
-                    path="/cohort6-schedule" 
-                    element={<Cohort6Schedule />} 
-                />
+                <Route path="/cohort6-schedule" element={<Cohort6Schedule />} />
 
                 {/* UPDATE THIS */}
                 {/* UPDATE THIS */}
@@ -76,8 +37,6 @@ function App() {
                 /> */}
                 {/* UPDATE THIS */}
                 {/* UPDATE THIS */}
-
-
             </Routes>
         </Router>
     );
